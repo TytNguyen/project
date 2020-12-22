69d5a920f95e8a67a88a33d94b3643293c97b022
TytNguyen <37128780+TytNguyen@users.noreply.github.com>
2020-12-22 18:03:45 +0700

    Update MatchingManager.js

--- app/manager/MatchingManager.js
+++ app/manager/MatchingManager.js
@@ -470,9 +470,9 @@ module.exports = {
 
     update: function (accessUserId, accessUserType, matchingId, updateData, callback) {
         try {
-            if ( accessUserType < Constant.USER_TYPE.MODERATOR && (updateData.step !== 2 || updateData.step !== 10) ) {
-                return callback(1, 'invalid_user_type', 403, null, null);
-            }
+            //if ( accessUserType < Constant.USER_TYPE.MODERATOR && (updateData.step !== 2 || updateData.step !== 10) ) {
+            //    return callback(1, 'invalid_user_type', 403, null, null);
+            //}
 
             let queryObj = {};
             let where = {};
